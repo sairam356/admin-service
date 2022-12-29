d6919ee1bd20b87fbd458ff731e884dd264532b8
Sai Ram <g.sairam67@gmail.com>
2022-12-29 14:04:52 +0530

    Update ExternalServiceCalls.java

--- src/main/java/com/example/demo/service/ExternalServiceCalls.java
+++ src/main/java/com/example/demo/service/ExternalServiceCalls.java
@@ -78,8 +78,10 @@ public class ExternalServiceCalls {
 				//	logger.info("Thread " + Thread.currentThread().getName());
 					HttpHeaders headers = new HttpHeaders();
 					headers.add("Authorization", "Bearer " + userInfo.getAccess_token());
+					
 					 HttpEntity request = new HttpEntity(userInfo,headers);
-					return restTemplate.postForObject(debitURL, request, Map.class);
+					//return restTemplate.postForObject(debitURL, request, Map.class);
+					return restTemplate.postForObject("https://debit-service-test-kushalbajji-dev.apps.sandbox.x8i5.p1.openshiftapps.com/debit", request, Map.class);
 				}
 			});
 		} catch (Exception e) {
